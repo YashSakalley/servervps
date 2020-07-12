2c6bcac9a98417bf349720d08d2e1488220e8f39
Shashank07-creator <53191212+Shashank07-creator@users.noreply.github.com>
2020-07-13 00:28:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,55 +1,102 @@
-var express = require('express'),
-    cors = require('cors'),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose');
-
-// Routes
-var reportRouter = require('./routes/report'),
-    indexRouter = require('./routes/index'),
-    verifyRouter = require('./routes/verify'),
-    userRouter = require('./routes/Persons/user'),
-    shoRouter = require('./routes/Persons/sho'),
-    ioRouter = require('./routes/Persons/io'),
-    spRouter = require('./routes/Persons/sp'),
-    firRouter = require('./routes/fir');
-
-const app = express(),
-    PORT = process.env.PORT || 5000;
-
-// App setup
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Mongoose setup
-mongoose.connect("mongodb://localhost/vps",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then((data) => {
-        console.log("DB connected successfully");
-    })
-    .catch((err) => {
-        console.log("Error connecting to DB : ", err);
-    })
-
-
-app.use('/firs', firRouter);
-app.use('/reports', reportRouter);
-app.use('/verify', verifyRouter);
-
-// Persons Router
-app.use('/user', userRouter);
-app.use('/sho', shoRouter);
-app.use('/sp', spRouter);
-app.use('/io', ioRouter);
-
-// Index Router
-app.use('/', indexRouter);
-
-
-app.listen(PORT, () => {
-    console.log('Server listening on PORT:', PORT);
+var wrapper = document.getElementById("signature-pad");
+var clearButton = wrapper.querySelector("[data-action=clear]");
+var changeColorButton = wrapper.querySelector("[data-action=change-color]");
+var undoButton = wrapper.querySelector("[data-action=undo]");
+var savePNGButton = wrapper.querySelector("[data-action=save-png]");
+var saveJPGButton = wrapper.querySelector("[data-action=save-jpg]");
+var saveSVGButton = wrapper.querySelector("[data-action=save-svg]");
+var canvas = wrapper.querySelector("canvas");
+var signaturePad = new SignaturePad(canvas, {
+  // It's Necessary to use an opaque color when saving image as JPEG;
+  // this option can be omitted if only saving as PNG or SVG
+  backgroundColor: 'rgb(255, 255, 255)'
+});
+// Adjust canvas coordinate space taking into account pixel ratio,
+// to make it look crisp on mobile devices.
+// This also causes canvas to be cleared.
+function resizeCanvas() {
+  // When zoomed out to less than 100%, for some very strange reason,
+  // some browsers report devicePixelRatio as less than 1
+  // and only part of the canvas is cleared then.
+  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+  // This part causes the canvas to be cleared
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+  // This library does not listen for canvas changes, so after the canvas is automatically
+  // cleared by the browser, SignaturePad#isEmpty might still return false, even though the
+  // canvas looks empty, because the internal data of this library wasn't cleared. To make sure
+  // that the state of this library is consistent with visual state of the canvas, you
+  // have to clear it manually.
+  signaturePad.clear();
+}
+// On mobile devices it might make more sense to listen to orientation change,
+// rather than window resize events.
+window.onresize = resizeCanvas;
+resizeCanvas();
+function download(dataURL, filename) {
+  var blob = dataURLToBlob(dataURL);
+  var url = window.URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  a.style = "display: none";
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+// One could simply use Canvas#toBlob method instead, but it's just to show
+// that it can be done using result of SignaturePad#toDataURL.
+function dataURLToBlob(dataURL) {
+  // Code taken from https://github.com/ebidel/filer.js
+  var parts = dataURL.split(';base64,');
+  var contentType = parts[0].split(":")[1];
+  var raw = window.atob(parts[1]);
+  var rawLength = raw.length;
+  var uInt8Array = new Uint8Array(rawLength);
+  for (var i = 0; i < rawLength; ++i) {
+    uInt8Array[i] = raw.charCodeAt(i);
+  }
+  return new Blob([uInt8Array], { type: contentType });
+}
+clearButton.addEventListener("click", function (event) {
+  signaturePad.clear();
+});
+undoButton.addEventListener("click", function (event) {
+  var data = signaturePad.toData();
+  if (data) {
+    data.pop(); // remove the last dot or line
+    signaturePad.fromData(data);
+  }
+});
+changeColorButton.addEventListener("click", function (event) {
+  var r = Math.round(Math.random() * 255);
+  var g = Math.round(Math.random() * 255);
+  var b = Math.round(Math.random() * 255);
+  var color = "rgb(" + r + "," + g + "," + b +")";
+  signaturePad.penColor = color;
+});
+savePNGButton.addEventListener("click", function (event) {
+  if (signaturePad.isEmpty()) {
+    alert("Please provide a signature first.");
+  } else {
+    var dataURL = signaturePad.toDataURL();
+    download(dataURL, "signature.png");
+  }
+});
+saveJPGButton.addEventListener("click", function (event) {
+  if (signaturePad.isEmpty()) {
+    alert("Please provide a signature first.");
+  } else {
+    var dataURL = signaturePad.toDataURL("image/jpeg");
+    download(dataURL, "signature.jpg");
+  }
+});
+saveSVGButton.addEventListener("click", function (event) {
+  if (signaturePad.isEmpty()) {
+    alert("Please provide a signature first.");
+  } else {
+    var dataURL = signaturePad.toDataURL('image/svg+xml');
+    download(dataURL, "signature.svg");
+  }
 });
